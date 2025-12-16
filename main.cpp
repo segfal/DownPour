5d39ea46152efdd15d043afa259501828e52db03
SegFal <segfaultboi@gmail.com>
2025-12-15 21:37:04 -0500

    Add glad and glfw submodules
    READ:
    to make sure glad is included properly,
    you must run the python script located at
    headers/glad/src/glad/glad.py with the following command:
    
    `python headers/glad/src/glad/glad.py --generator=c --spec=gl --out-path=./headers/glad --extensions="GL_ARB_separate_shader_objects GL_ARB_shader_objects GL_ARB_vertex_shader GL_ARB_fragment_shader"`

---
+++ main.cpp
@@ -0,0 +1,64 @@
+#include <glad/gl.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
+
+
+void processInput(GLFWwindow* window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+int main() {
+    
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+        return -1;
+    }
+
+    
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); 
+
+    
+    GLFWwindow* window = glfwCreateWindow(800, 600, "DownPour", nullptr, nullptr);
+    if (!window) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    
+    
+    if (!gladLoadGL(glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    
+    while (!glfwWindowShouldClose(window)) {
+        
+        processInput(window);
+
+        
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    
+    glfwTerminate();
+    return 0;
+}
